f0d1c5dc4aadb53ee76b2d121115ad3f977e0c80
ali404 <acerrahoglu.ali@gmail.com>
2016-06-06 01:54:01 +0300

    Added basic test

---
+++ karma.conf.js
@@ -0,0 +1,30 @@
+const webpack = require('webpack')
+
+module.exports = function(config) {
+    config.set({
+        browsers: ['Chrome'],
+        singleRun: true,
+        frameworks: ['mocha'],
+        files: [
+            'tests.webpack.js'
+        ],
+        preprocessors: {
+            'tests.webpack.js': ['webpack', 'sourcemap']
+        },
+        reporters: ['dots'],
+        webpack: {
+            devtool: 'inline-source-map',
+            module: {
+                loaders: [
+                    {
+                        test: /\.js$/,
+                        loader: 'babel-loader'
+                    }
+                ]
+            }
+        },
+        webpackServer: {
+            noInfo: true
+        }
+    })
+}
